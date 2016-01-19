2ae949abd9ad2ee07eddfcdf3d0d5d932a581111
Gnnng <gnnnnng@gmail.com>
2016-01-19 21:00:42 +0800

    feat: add an example of defer

--- 5_Defer.go
+++ 5_Defer.go
@@ -11,11 +11,26 @@ func f() (ret int) {
 	//  will return 1
 }
 
+func un(s string) {
+	fmt.Println("Leaving func", s)
+}
+
+func trace(s string) (ret string) {
+	fmt.Println("Entering func", s)
+	return s
+}
+
+func hello() {
+	// The parameter of deferred function will be executed right away
+	defer un(trace("hello"))
+	fmt.Println("Hello")
+}
+
 func main() {
 	for i := 0; i < 5; i++ {
 		defer fmt.Printf("%d ", i) // Multiple deferred lines obey the rule of FIFO
 		// will output 4 3 2 1 0
 	}
 	fmt.Println(f())
-
+	hello()
 }
